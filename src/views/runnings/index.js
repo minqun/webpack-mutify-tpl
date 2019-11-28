b743f2800297b34eb0075738f712d65ec2d75fa8
chenminqun <c1029mq@qq.com>
2019-11-28 14:58:28 +0800

    feat 添加模板设置

--- src/views/runnings/index.js
+++ src/views/runnings/index.js
@@ -4,3 +4,10 @@ import "@source/js/index";
 // import React from 'react'
 // import ReactDom from 'react-dom'
 // ReactDom.render(<h1> Hello, world! </h1>, document.getElementById('root'))
+function getSize() {
+  let deviceW = $("body").width();
+  let deviceWidth = 780;
+  $("html").attr("fontSize", `${deviceW / deviceWidth * 100}px`);
+}
+getSize();
+window.onresize = getSize;
--- src/views/runnings/test.js
+++ src/views/runnings/test.js
@@ -1,2 +1,57 @@
-import name from "@source/css/test";
-// import "@source/js/test";
+// import name from "@source/css/test";
+// // 设置大小
+// function getSize() {
+//   let deviceW = $("body").width();
+//   let deviceWidth = 780;
+//   $("html").attr("fontSize", `${deviceW / deviceWidth * 100}px`);
+// }
+// getSize();
+// window.onresize = getSize;
+
+class Node {
+  constructor(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+  show() {
+    console.log(this.data);
+  }
+}
+class tree {
+  constructor() {
+    this.root = null;
+  }
+  insert(data) {
+    var node = new Node(data, null, null);
+    if (!this.root) {
+      this.root = node;
+      return;
+    }
+    var current = this.root;
+    var parent = null;
+    while (current) {
+      parent = current;
+      if (data > parent.data) {
+        current = current.left;
+        // if (!current) {
+        //   parent.left = node;
+        //   return;
+        // }
+      } else {
+        current = current.right;
+        // if (!current) {
+        //   parent.right = node;
+        //   return;
+        // }
+      }
+    }
+  }
+}
+let t = new tree();
+t.insert(4);
+t.insert(2);
+t.insert(3);
+t.insert(1);
+t.insert(5);
+console.log(t);
