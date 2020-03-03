73a8153f25dfa43d0f9be3a5503a0bcb95ebe8c9
chenminqun <c1029mq@qq.com>
2020-03-03 18:47:07 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,14 @@
 const path = require('path');
 
 const webpackMerge = require('webpack-merge');
-const BaseConfig = require("./webpack.base");
+const BaseConfig = require('./webpack.base');
 
-let config = webpackMerge({},
-    BaseConfig, {
-
-        devtool: 'inline-source-map',
-        devServer: {
-            host: '192.168.4.81',
-            port: '8066',
-            contentBase: path.join(__dirname, "dist"),
-        },
-
-    })
-module.exports = config
+let config = webpackMerge({}, BaseConfig, {
+    devtool: 'inline-source-map',
+    devServer: {
+        host: '192.168.5.120',
+        port: '8066',
+        contentBase: path.join(__dirname, 'dist')
+    }
+});
+module.exports = config;
